f5671c89e38d646eec10fa1b95deac7dd349c1e1
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2019-01-27 12:33:56 +0900

    Implement SilentActor

--- chapter-testdriven/src/test/scala/aia/testdriven/SilentActor01Test.scala
+++ chapter-testdriven/src/test/scala/aia/testdriven/SilentActor01Test.scala
@@ -1,7 +1,7 @@
 package aia.testdriven
 
-import org.scalatest.{WordSpecLike, MustMatchers}
-import akka.testkit.TestKit
+import org.scalatest.{MustMatchers, WordSpecLike}
+import akka.testkit.{TestActorRef, TestKit}
 import akka.actor._
 
 //This test is ignored in the BookBuild, it's added to the defaultExcludedNames
@@ -10,35 +10,45 @@ class SilentActor01Test extends TestKit(ActorSystem("testsystem"))
   with WordSpecLike
   with MustMatchers
   with StopSystemAfterAll {
-  // travisのビルドをパスするためコメントアウトしますが、こちらが書籍内のテストです。
-  // "A Silent Actor" must {
-  //   "change state when it receives a message, single threaded" in {
-  //     // テストを書くと最初は失敗する
-  //     fail("not implemented yet")
-  //   }
-  //   "change state when it receives a message, multi-threaded" in {
-  //     // テストを書くと最初は失敗する
-  //     fail("not implemented yet")
-  //   }
-  // }
   "A Silent Actor" must {
-    "change state when it receives a message, single threaded" ignore {
-      // テストを書くと最初は失敗する
-      fail("not implemented yet")
+    "change state when it receives a message, single threaded" in {
+      import SilentActor._
+      val silentActor = TestActorRef[SilentActor]
+      silentActor ! SilentMessage("whisper")
+      silentActor.underlyingActor.state must contain("whisper")
     }
-    "change state when it receives a message, multi-threaded" ignore {
-      // テストを書くと最初は失敗する
-      fail("not implemented yet")
+    "change state when it receives a message, multi-threaded" in {
+      import SilentActor._
+      val silentActor = system.actorOf(Props[SilentActor], "s3")
+      silentActor ! SilentMessage("whisper1")
+      silentActor ! SilentMessage("whisper2")
+      silentActor ! GetState(testActor)
+
+      expectMsg(Vector("whisper1", "whisper2"))
     }
   }
 
 }
 
+object SilentActor {
 
+  case class SilentMessage(data: String)
+
+  case class GetState(receiver: ActorRef)
+
+}
 
 class SilentActor extends Actor {
+
+  import SilentActor._
+
+  var internalState: Vector[String] = Vector[String]()
+
   def receive = {
-    case msg =>
+    case SilentMessage(data) => internalState = internalState :+ data
+    case GetState(receiver) => receiver ! internalState
   }
+
+  def state = internalState
 }
 
